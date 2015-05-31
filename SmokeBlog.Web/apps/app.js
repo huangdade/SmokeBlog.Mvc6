3b87acb857c0fb2c75e4e66a6635fbbf4987a34b
scheshan <5373827@qq.com>
2015-05-31 15:44:13 +0800

    添加文件

--- SmokeBlog.Web/apps/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('blogAdmin', ['ngRoute', 'ngMessages', 'ui.bootstrap', 'blogAdmin.controllers', 'blogAdmin.directives']);
-angular.module('blogAdmin').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/', { templateUrl: '/apps/templates/dashboard.html', controller: 'mainCtrl' }).when('/userlist', { templateUrl: '/apps/templates/userlist.html', controller: 'userListCtrl' }).when('/profile', { templateUrl: '/apps/templates/my.html', controller: 'myCtrl' }).otherwise({ redirectTo: '/' });
-    $locationProvider.html5Mode(true);
-}]);
-angular.module('blogAdmin.services', []);
-angular.module('blogAdmin.directives', []);
-angular.module('blogAdmin.controllers', ['blogAdmin.services', 'ui.bootstrap']);
