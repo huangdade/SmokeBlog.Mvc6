eb35d768922327d45120efa2846f65e7da7be8db
何山 <5373827@qq.com>
2015-06-01 10:13:41 +0800

    添加文件

--- SmokeBlog.Web/Gulpfile.js
+++ SmokeBlog.Web/Gulpfile.js
@@ -7,19 +7,44 @@ var gulp = require('gulp');
 var copy = require('gulp-copy');
 var concat = require('gulp-concat');
 
+gulp.task('libs', function () {
+    gulp.src([
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/angularjs/angular.min.js',
+        'bower_components/angular-route/angular-route.min.js',
+        'bower_components/angular-messages/angular-messages.min.js',
+        'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+        'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        'bower_components/lodash/lodash.min.js',
+        'bower_components/messenger/build/js/messenger.min.js',
+        'bower_components/jquery.validate/dist/jquery.validate.min.js'
+    ]).pipe(gulp.dest('wwwroot/libs/'));
+
+    gulp.src([
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'bower_components/fontawesome/css/font-awesome.min.css',
+        'bower_components/messenger/build/css/messenger.css',
+        'bower_components/messenger/build/css/messenger-theme-future.css'
+    ]).pipe(gulp.dest('wwwroot/styles/'));
+
+    gulp.src([
+        'bower_components/fontawesome/fonts/*'
+    ]).pipe(gulp.dest('wwwroot/fonts'));
+});
+
 gulp.task('admin', function () {
-    var styleFiles = [
+    //concat css files
+    gulp.src([
         'bower_components/bootstrap/dist/css/bootstrap.min.css',
         'bower_components/fontawesome/css/font-awesome.min.css',
         'bower_components/messenger/build/css/messenger.css',
         'bower_components/messenger/build/css/messenger-theme-future.css',
         'styles/app.css'
-    ];
+    ]).pipe(concat('admin.css')).pipe(gulp.dest('wwwroot/styles'));
 
-    gulp.src(styleFiles).pipe(gulp.dest('wwwroot/styles'));
-    gulp.src(styleFiles).pipe(concat('app_all.css')).pipe(gulp.dest('wwwroot/styles'));
-
-    var scriptFiles = [
+    //concat javascript files
+    gulp.src([
         'bower_components/jquery/dist/jquery.min.js',
         'bower_components/bootstrap/dist/js/bootstrap.min.js',
         'bower_components/angularjs/angular.min.js',
@@ -33,18 +58,29 @@ gulp.task('admin', function () {
         'apps/directives/*.js',
         'apps/services/*.js',
         'apps/controllers/*.js'
-    ]
+    ]).pipe(concat('admin.js')).pipe(gulp.dest('wwwroot/scripts'));
 
-    gulp.src(scriptFiles).pipe(gulp.dest('wwwroot/scripts'));
-    gulp.src(scriptFiles).pipe(concat('app_all.js')).pipe(gulp.dest('wwwroot/scripts'));
-
+    //move template files
     gulp.src([
         'apps/templates/*'
-    ])
-    .pipe(gulp.dest('wwwroot/templates'));
+    ]).pipe(gulp.dest('wwwroot/templates'));
 
+    //move css files for debug
     gulp.src([
-        'bower_components/fontawesome/fonts/*'
-    ])
-    .pipe(gulp.dest('wwwroot/fonts'));
+        'styles/app.css'
+    ]).pipe(gulp.dest('wwwroot/styles/admin/'));
+
+    //move javascript files for debug
+    gulp.src('apps/*.js').pipe(gulp.dest('wwwroot/scripts/admin'));
+    gulp.src('apps/directives/*.js').pipe(gulp.dest('wwwroot/scripts/admin/directives'));
+    gulp.src('apps/services/*.js').pipe(gulp.dest('wwwroot/scripts/admin/services'));
+    gulp.src('apps/controllers/*.js').pipe(gulp.dest('wwwroot/scripts/admin/controllers'));
+});
+
+gulp.task('account', function () {
+    gulp.src('styles/account.css').pipe(gulp.dest('wwwroot/styles/'));
+});
+
+gulp.task('all', ['libs', 'admin', 'account'], function () {
+
 });
