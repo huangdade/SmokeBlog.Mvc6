b9a0174b7741d35c3d07371c01c8ff0308a43ec4
何山 <5373827@qq.com>
2015-06-12 11:48:53 +0800

    文章功能

---
+++ SmokeBlog.Web/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmokeBlog.Web.Controllers
+{
+    [Route("")]
+    public class ArticleController : Controller
+    {
+        public IActionResult QueryByCategory()
+        {
+            return this.View();
+        }
+
+        public IActionResult QueryByAuthor()
+        {
+            return this.View();
+        }
+    }
+}
